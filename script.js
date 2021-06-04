9834efd318b358b2351e4bde59a558ea662e496f
axieax <x.and.i.rew.e@gmail.com>
2021-06-04 21:28:52 +1000

    updated syntax

--- script.js
+++ script.js
@@ -18,107 +18,105 @@ const hideResponses = false;
 const maxTextLength = 1024;
 const maxFields = 25;
 
-// Main Function
-function submitPost(e) {
-    // retrieve and unpack data
-    let responses = e.response.getItemResponses();
+// Called Function
+const submitPost = (e) => {
+  // retrieve and unpack data
+  const responses = e.response.getItemResponses();
 
-    // extract responses
-    let payload = [];
-    extractResponses(payload, responses);
-    
-    // ignore empty responses
-    if (!payload.length) return;
+  // extract responses
+  const payload = extractResponses(responses);
+  // ignore empty responses
+  if (!payload.length) return;
 
-    // prepare POST request to webhook
-    let formTitle = e.source.getTitle();
-    if (!formTitle) formTitle = 'Untitled form';
-    let embed = {
-        'title': '✨ ' + formTitle + ' has received a new response!',
-        'footer': {
-            'text': 'Google Forms to Discord Automation - www.github.com/axieax',
-        },
-        'color': 16766720,
-    }
-    // include responses in payload unless specified otherwise
-    if (!hideResponses) embed.fields = payload;
-    
-    // create POST request to webhook
-    let options = {
-        'method': 'POST',
-        'headers': {
-            'Content-Type': 'application/json',
-        },
-        'payload': JSON.stringify({
-            'username': 'Response Carrier',
-            'avatar_url': 'https://github.com/axieax/google-forms-to-discord/blob/main/assets/birb.jpg?raw=true',
-            'embeds': [embed],
-        }),
-    };
-    UrlFetchApp.fetch(webhookURL, options);
+  // prepare POST request to webhook
+  const formTitle = e.source.getTitle() ?? 'Untitled Form';
+  const embed = {
+    'title': '✨ ' + formTitle + ' has received a new response!',
+    'footer': {
+      'text': 'Google Forms to Discord Automation - https://github.com/axieax',
+    },
+    'color': 16766720,
+  }
+  // include responses in payload unless specified otherwise
+  if (!hideResponses) embed.fields = payload;
+
+  // create POST request to webhook
+  let options = {
+    'method': 'POST',
+    'headers': {
+      'Content-Type': 'application/json',
+    },
+    'payload': JSON.stringify({
+      'username': 'Response Carrier',
+      'avatar_url': 'https://github.com/axieax/google-forms-to-discord/blob/main/assets/birb.jpg?raw=true',
+      'embeds': [embed],
+    }),
+  };
+  UrlFetchApp.fetch(webhookURL, options);
 }
 
 
 // Extract Responses
-function extractResponses(payload, responses) {
-    responses.forEach(response => {
-        let item = response.getItem();
-        let resp = response.getResponse();
-        let respFmt;
-        switch (item.getType()) {
-            case FormApp.ItemType.CHECKBOX:
-                // Checkbox
-                respFmt = '';
-                resp.forEach(option => {
-                    respFmt += option + '\n';
-                })
-                resp = respFmt;
-                break;
-            case FormApp.ItemType.FILE_UPLOAD:
-                // File upload
-                resp = 'File(s) uploaded';
-                break;
-            case FormApp.ItemType.GRID:
-                // Multiple choice grid
-                respFmt = '';
-                rows = item.asGridItem().getRows();
-                for (i = 0; i < resp.length; i++) {
-                    // skip empty rows?
-                    respFmt += rows[i] + ': ' + resp[i] + '\n';
-                }
-                resp = respFmt; // new lines
-                break;
-            case FormApp.ItemType.CHECKBOX_GRID:
-                // Tick box grid
-                respFmt = '';
-                rows = item.asCheckboxGridItem().getRows();
-                for (i = 0; i < resp.length; i++) {
-                    // skip empty rows?
-                    respFmt += rows[i] + ': ' + resp[i] + '\n';
-                }
-                resp = respFmt; // new lines
-                break;
-            default:
-                // Short answer, paragraph, multiple choice, linear scale, date, time
-                break;
+const extractResponses = (responses) => {
+  let payload = [];
+  responses.forEach(response => {
+    const item = response.getItem();
+    let resp = response.getResponse();
+    let respFmt;
+    switch (item.getType()) {
+      case FormApp.ItemType.CHECKBOX:
+        // Checkbox
+        resp = resp.join('\n');
+        break;
+      case FormApp.ItemType.FILE_UPLOAD:
+        // File upload
+        resp = 'File(s) uploaded';
+        break;
+      case FormApp.ItemType.GRID:
+        // Multiple choice grid
+        respFmt = [];
+        rows = item.asGridItem().getRows();
+        for (i = 0; i < resp.length; i++) {
+          // skip empty rows?
+          respFmt.push(rows[i] + ': ' + resp[i])
         }
-        
-        // ignore empty responses
-        if (resp) payload.push({
-            'name': item.getTitle(),
-            'value': (resp.length <= maxTextLength) ? resp : resp.slice(0, maxTextLength - 3) + '...',
-            'inline': false,
-        });
+        resp = respFmt.join('\n');
+        break;
+      case FormApp.ItemType.CHECKBOX_GRID:
+        // Tick box grid
+        respFmt = [];
+        rows = item.asCheckboxGridItem().getRows();
+        for (i = 0; i < resp.length; i++) {
+          // skip empty rows?
+          respFmt.push(rows[i] + ': ' + resp[i])
+        }
+        resp = respFmt.join('\n');
+        break;
+      default:
+        // Short answer, paragraph, multiple choice, linear scale, date, time
+        break;
+    }
+
+    // ignore empty responses
+    if (resp) payload.push({
+      'name': item.getTitle(),
+      'value': (resp.length <= maxTextLength) ? resp : resp.slice(0, maxTextLength - 3) + '...',
+      'inline': false,
     });
+  });
+  return payload;
 }
 
 
 /* Future Features:
 Embed Limits (https://discord.com/developers/docs/resources/channel#embed-limits)
-    - Embed Title (includes Form Title) - Maximum 256 characters
-    - Payload Fields (includes Payload Responses) - Maximum 25 responses can be displayed
-    - Field Name (includes Response Question) - Maximum 256 characters
-    - Total Characters (includes Embed Title, Field Names, Field Values, Footer Text) - Maximum 6000 characters
-        - https://developers.google.com/apps-script/reference/forms/form-response#toprefilledurl
+  - Embed Title (includes Form Title) - Maximum 256 characters
+  - Payload Fields (includes Payload Responses) - Maximum 25 responses can be displayed
+  - Field Name (includes Response Question) - Maximum 256 characters
+  - Total Characters (includes Embed Title, Field Names, Field Values, Footer Text) - Maximum 6000 characters
+      - https://developers.google.com/apps-script/reference/forms/form-response#toprefilledurl
 Regex?
+Links to files uploaded
+Date format
+Fix ellipsis
 */
