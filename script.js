507bfb809520a6f4594bcebf3e679cea55861af7
Andrew <x.and.i.rew.e@gmail.com>
2023-08-07 22:25:28 +1000

    chore(script): clean up code

--- script.js
+++ script.js
@@ -8,13 +8,12 @@
 */
 
 // [TODO]: Paste your Discord Webhook URL in the quotation marks below (don't remove the quotation marks)
-const webhookURL = '';
+const webhookURL = "";
 // [OPTIONAL]: If you want your responses to be hidden in the notification, change false to true below
 const hideResponses = false;
 // [OPTIONAL]: If you want to show incomplete rows for grids and checkbox grids, change true to false below
 const hideEmptyRows = true;
 // Further setup instructions can be found at https://github.com/axieax/google-forms-to-discord/
-
 
 /*
   DO NOT MODIFY BELOW
@@ -24,18 +23,20 @@ const hideEmptyRows = true;
 const maxTextLength = 1024;
 const maxFields = 25;
 
-// function called on form submit
-const submitPost = (e) => {
+/**
+ * Post Google Form responses to Discord Webhook
+ */
+function submitPost(e) {
   // prepare POST request to webhook
-  const formTitle = e.source.getTitle() ?? 'Untitled Form';
+  const formTitle = e.source.getTitle() ?? "Untitled Form";
   const embed = {
-    'title': `✨ ${formTitle} has received a new response!`,
-    'footer': {
-      'text': 'Google Forms to Discord Automation - https://github.com/axieax',
+    title: `✨ ${formTitle} has received a new response!`,
+    footer: {
+      text: "Google Forms to Discord Automation - https://github.com/axieax",
     },
-    'color': 16766720,
-  }
-  
+    color: 16766720,
+  };
+
   // retrieve and unpack data
   const responses = e.response.getItemResponses();
   // format responses if responses are not to be hidden in the webhook
@@ -50,78 +51,86 @@ const submitPost = (e) => {
 
   // create POST request to webhook
   const options = {
-    'method': 'POST',
-    'headers': {
-      'Content-Type': 'application/json',
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-    'payload': JSON.stringify({
-      'username': 'Response Carrier',
-      'avatar_url': 'https://github.com/axieax/google-forms-to-discord/blob/main/assets/birb.jpg?raw=true',
-      'embeds': [embed],
+    payload: JSON.stringify({
+      username: "Response Carrier",
+      avatar_url:
+        "https://github.com/axieax/google-forms-to-discord/blob/main/assets/birb.jpg?raw=true",
+      embeds: [embed],
     }),
   };
   UrlFetchApp.fetch(webhookURL, options);
-};
+}
 
-
-// extract responses
+/**
+ * Extract responses
+ * @return {Array<string>}
+ */
 const extractResponses = (responses) => {
+  const formatGridResponse = (response, questions) => {
+    return response
+      .map((answer, index) => {
+        if (!hideEmptyRows || answer !== null) {
+          return `${questions[index]}: ${
+            Array.isArray(answer) ? answer.join(", ") : answer
+          }`;
+        }
+      })
+      .filter((x) => x !== undefined)
+      .join("\n");
+  };
+
   // format each response
-  const payload = [];
-  responses.forEach(response => {
-    const item = response.getItem();
-    let resp = response.getResponse();
-    let respFmt, questions;
+  const payload = responses.map((r) => {
+    const item = r.getItem();
+    const response = r.getResponse();
+
+    let resp, questions;
     switch (item.getType()) {
       case FormApp.ItemType.CHECKBOX:
         // display checkbox responses on separate lines
-        resp = resp.join('\n');
+        resp = response.join("\n");
         break;
       case FormApp.ItemType.FILE_UPLOAD:
         // generate URL for uploaded files
-        resp = 'File(s) uploaded:\n' + resp.map(
-          fileID => 'https://drive.google.com/open?id=' + fileID
-        ).join('\n');
+        resp =
+          "File(s) uploaded:\n" +
+          response
+            .map((fileID) => "https://drive.google.com/open?id=" + fileID)
+            .join("\n");
         break;
       case FormApp.ItemType.GRID:
         // display grid responses on separate lines
-        respFmt = []
         questions = item.asGridItem().getRows();
-        resp.forEach((answer, index) => {
-          // exclude empty responses unless specified otherwise
-          if (!hideEmptyRows || answer !== null)
-            respFmt.push(`${questions[index]}: ${Array.isArray(answer) ? answer.join(', ') : answer}`);
-        });
-        resp = respFmt.join('\n');
+        resp = formatGridResponse(response, questions);
         break;
       case FormApp.ItemType.CHECKBOX_GRID:
         // display grid responses on separate lines
-        respFmt = []
         questions = item.asCheckboxGridItem().getRows();
-        resp.forEach((answer, index) => {
-          // exclude empty responses unless specified otherwise
-          if (!hideEmptyRows || answer !== null)
-            respFmt.push(`${questions[index]}: ${Array.isArray(answer) ? answer.join(', ') : answer}`);
-        });
-        resp = respFmt.join('\n');
+        resp = formatGridResponse(response, questions);
         break;
       default:
         // short answer, paragraph, multiple choice, linear scale, datetime
+        resp = response;
         break;
     }
 
-    // ignore empty responses
-    if (resp) payload.push({
-      'name': item.getTitle(),
-      'value': (resp.length <= maxTextLength) ? resp : resp.slice(0, maxTextLength - 3) + '...',
-      'inline': false,
-    });
+    return {
+      name: item.getTitle(),
+      value:
+        resp.length <= maxTextLength
+          ? resp
+          : resp.slice(0, maxTextLength - 3) + "...",
+      inline: false,
+    };
   });
 
-  // TODO: maxFields
+  // TODO: handle maxFields
   return payload;
 };
-
 
 /* Future Features:
 Embed Limits (https://discord.com/developers/docs/resources/channel#embed-limits)
@@ -133,5 +142,4 @@ Embed Limits (https://discord.com/developers/docs/resources/channel#embed-limits
 Regex?
 Date format
 Extend ellipsis
-Update instructions for non-legacy (new) script editor
 */
